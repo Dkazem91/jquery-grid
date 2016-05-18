1235ca084d24afac15eaec15fa27439ab1b58344
Daniel Kazemian <dkazemian@gmail.com>
2016-05-17 22:00:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 $(document).ready(function() {
-  $('button').on('click',function(event){
+  $('button').on('click', function(event) {
     event.preventDefault();
-    $('.unit').css("background-color","white");
-    
+    $('.unit').css("background-color", "white");
+
   });
-  $('#submit').on('click',function(event){
+  $('#submit').on('click', function(event) {
     event.preventDefault();
     $('.unit').remove();
-    var input=$('#size').val();
-    if(input>100){
+    var input = $('#size').val();
+    if (input > 100) {
       $('p').remove()
       $('body').append("<p>size is too great!</p>")
-    }else{
-    $('p').remove();
+    } else {
+      $('p').remove();
       var unit = $("<div class='unit'></div>");
-      
-      for(var x = 0; x < (input*input); x++) {
-        
-          
-              unit = $("<div class='unit'></div>");
-            unit.appendTo('#container'); 
-         $('.unit').height((480/input)-2)
-          $('.unit').width((480/input)-2)
-        $('.unit').hover(function(){
-        $(this).css("background-color","black");
- });
-    
-      }
-       
-    }
-        
-    });
 
+      for (var x = 0; x < (input * input); x++) {
+
+        unit = $("<div class='unit'></div>");
+        unit.appendTo('#container');
+
+      }
+
+    }
+    $('.unit').height((480 / input) - 2)
+    $('.unit').width((480 / input) - 2)
+    $('.unit').hover(function() {
+      $(this).css("background-color", "black");
+    });
+  });
 
 });
